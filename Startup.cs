21c3fcf3368cc9d6c8bc8149630bed97b5b5f716
kevinmenezes <kevinmenezes@yahoo.com>
2019-12-17 21:12:18 -0800

    added SSL url setting

--- Startup.cs
+++ Startup.cs
@@ -68,7 +68,7 @@ namespace survey
                 app.UseHsts();
             }
 
-            app.UseCors(options => options.WithOrigins("http://localhost:4200", "http://review.inamdo.com", "http://message.inamdo.com").AllowAnyMethod().AllowAnyHeader());
+            app.UseCors(options => options.WithOrigins("http://localhost:56197", "http://review.inamdo.com", "http://message.inamdo.com").AllowAnyMethod().AllowAnyHeader());
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
